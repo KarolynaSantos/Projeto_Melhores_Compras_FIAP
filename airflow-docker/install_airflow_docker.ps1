ef245d5964ff88d8748cd2066b488d4223f84a1e
KarolynaSantos <karolyna.fernanda1001@gmail.com>
2024-11-04 16:21:20 -0300

    fix: apagar arquivo

--- airflow-docker/install_airflow_docker.ps1
+++
@@ -1,7 +0,0 @@
-# Criar diretórios necessários
-New-Item -ItemType Directory -Path .\dags, .\logs, .\plugins, .\config, .\scripts -Force
-
-# Iniciar o Airflow
-docker-compose up -d airflow-init
-
-docker-compose up -d
